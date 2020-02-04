793ad02fa6c0671340d4d022d12376521bd2ab5c
Reid Chan <reidchan@apache.org>
2020-02-04 11:50:46 +0800

    Rename GrantPermission to GrantRevokePermission && Add revoke functions

--- toy-hbase/src/main/java/org/apache/toy/GrantRevokePermission.java
+++ toy-hbase/src/main/java/org/apache/toy/GrantRevokePermission.java
@@ -29,8 +29,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class GrantPermission extends AbstractHBaseToy {
+public class GrantRevokePermission extends AbstractHBaseToy {
 
+  @SuppressWarnings("rawtypes")
+  private final Parameter<Enum> grant_revoke =
+      Parameter.<Enum>newBuilder()
+               .setKey("grant_or_revoke").setType(G_V.class)
+               .setRequired(true).setDescription("Grant or revoke action")
+               .setDefaultValue(G_V.G)
+               .opt();
   private final Parameter<String> user_name =
       Parameter.<String>newBuilder()
                .setKey("user_name").setType(String.class)
@@ -64,13 +71,19 @@ public class GrantPermission extends AbstractHBaseToy {
                .setDescription("Grant namespace permissions.")
                .opt();
 
+  enum G_V {
+    G, V
+  }
+
   private Connection connection;
   private TableName table;
   private Permission.Action[] actions;
   private String[] columns;
+  private G_V act;
 
   @Override
   protected void requisite(@SuppressWarnings("rawtypes") List<Parameter> requisites) {
+    requisites.add(grant_revoke);
     requisites.add(user_name);
     requisites.add(table_name);
     requisites.add(permissions);
@@ -84,6 +97,7 @@ public class GrantPermission extends AbstractHBaseToy {
     table      = TableName.valueOf(table_name.value());
     actions    = extractPermissionActions(permissions.value());
     columns    = columns_permissions.empty() ? null : columns_permissions.value();
+    act        = (G_V)grant_revoke.value();
   }
 
   private Permission.Action[] extractPermissionActions(String permission_actions) {
@@ -103,47 +117,70 @@ public class GrantPermission extends AbstractHBaseToy {
 
   @Override
   protected int haveFun() throws Exception {
+    if (!AccessControlClient.isAccessControllerRunning(connection)) return RETURN_CODE.FAILURE.code();
+
     try {
-           if (!administrator.unset())          grantGlobalPermissions();
-      else if (!namespace.empty())              grantNamespacePermissions();
+           if (!administrator.unset())          performGlobalPermissions(act);
+      else if (!namespace.empty())              performNamespacePermissions(act);
       else if (!columns_permissions.empty())
-               for (String column : columns)    grantTableColumnPermissions(column);
-      else                                      grantTablePermissions();
+               for (String column : columns)    performTableColumnPermissions(act, column);
+      else                                      performTablePermissions(act);
     } catch (Throwable throwable) {
       throw new RuntimeException(throwable);
     }
     return RETURN_CODE.SUCCESS.code();
   }
 
-  private void grantGlobalPermissions() throws Throwable {
-    AccessControlClient.grant(connection, user_name.value(), actions);
+  private void performGlobalPermissions(G_V act) throws Throwable {
+    switch (act) {
+      case G: AccessControlClient.grant(connection, user_name.value(), actions);  break;
+      case V: AccessControlClient.revoke(connection, user_name.value(), actions); break;
+      default:                                                                    break;
+    }
+
   }
 
-  private void grantNamespacePermissions() throws Throwable {
-    AccessControlClient.grant(connection, namespace.value(), user_name.value(), actions);
+  private void performNamespacePermissions(G_V act) throws Throwable {
+    switch (act) {
+      case G: AccessControlClient.grant(connection, namespace.value(), user_name.value(), actions);   break;
+      case V: AccessControlClient.revoke(connection, namespace.value(), user_name.value(), actions);  break;
+      default:                                                                                        break;
+    }
+
   }
 
-  private void grantTableColumnPermissions(String column) throws Throwable {
+  private void performTableColumnPermissions(G_V act, String column) throws Throwable {
     String[] family_qualifier = column.split(":");
     if (family_qualifier.length == 1) {
-      AccessControlClient.grant(connection,
-                                table,
-                                user_name.value(),
-                                Bytes.toBytes(family_qualifier[0]), null,
-                                actions);
+      switch (act) {
+        case G: AccessControlClient.grant(connection, table, user_name.value(),
+                                          Bytes.toBytes(family_qualifier[0]), null, actions);    break;
+        case V: AccessControlClient.revoke(connection, table, user_name.value(),
+                                          Bytes.toBytes(family_qualifier[0]), null, actions); break;
+        default:                                                                                      break;
+      }
+
     } else if (family_qualifier.length == 2) {
-      AccessControlClient.grant(connection,
-                                table,
-                                user_name.value(),
-                                Bytes.toBytes(family_qualifier[0]), Bytes.toBytes(family_qualifier[1]),
-                                actions);
+      switch (act) {
+        case G: AccessControlClient.grant(connection, table, user_name.value(),
+                                          Bytes.toBytes(family_qualifier[0]), Bytes.toBytes(family_qualifier[1]),
+                                          actions); break;
+        case V: AccessControlClient.revoke(connection, table, user_name.value(),
+                                          Bytes.toBytes(family_qualifier[0]), Bytes.toBytes(family_qualifier[1]),
+                                          actions); break;
+        default:                                    break;
+      }
     } else {
       throw new Throwable("Column " + column + " has problem.");
     }
   }
 
-  private void grantTablePermissions() throws Throwable {
-    AccessControlClient.grant(connection, table, user_name.value(), null, null, actions);
+  private void performTablePermissions(G_V act) throws Throwable {
+    switch (act) {
+      case G: AccessControlClient.grant(connection, table, user_name.value(), null, null, actions);     break;
+      case V: AccessControlClient.revoke(connection, table, user_name.value(), null, null, actions); break;
+      default:                                                                                                      break;
+    }
   }
 
   @Override
