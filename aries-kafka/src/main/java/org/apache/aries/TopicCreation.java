0b1b238826f06f7e9b27f3490f0130392fc22760
Reid Chan <reidchan@apache.org>
2020-07-14 17:15:18 +0800

    Rename all module name

---
+++ aries-kafka/src/main/java/org/apache/aries/TopicCreation.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.apache.aries.common.IntParameter;
+import org.apache.aries.common.Parameter;
+import org.apache.aries.common.ShortParameter;
+import org.apache.aries.common.StringArrayParameter;
+import org.apache.aries.common.StringParameter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class TopicCreation extends AbstractKafkaToy {
+
+  private final Parameter<String[]> topics =
+      StringArrayParameter.newBuilder("tc.topics_creating").setRequired().setDescription("topics to be created").opt();
+  private final Parameter<Integer> partitions =
+      IntParameter.newBuilder("tc.partitions").setRequired().setDescription("partitions for each topics").opt();
+  private final Parameter<Short> replications =
+      ShortParameter.newBuilder("tc.replications").setRequired().setDescription("replications for each partitions").opt();
+  private final Parameter<String> retention_ms =
+      StringParameter.newBuilder("tc.retention_ms").setDescription("retention time in ms for topics").opt();
+  private final Parameter<String> retention_bytes =
+      StringParameter.newBuilder("tc.retention_bytes").setDescription("retentions bytes for topics").opt();
+
+  private final Map<String, String> configs = new HashMap<>();
+
+  private AdminClient ac;
+  private int topic_partitions;
+  private short partition_replications;
+
+  @Override
+  protected void requisite(List<Parameter> requisites) {
+    requisites.add(topics);
+    requisites.add(partitions);
+    requisites.add(replications);
+    requisites.add(retention_ms);
+    requisites.add(retention_bytes);
+  }
+
+  @Override
+  protected void buildToy(ToyConfiguration configuration) throws Exception {
+    if (retention_ms.empty() || retention_bytes.empty()) {
+      throw new IllegalArgumentException(retention_ms.key() + " or " + retention_bytes.key() +
+          " at least one of these two must be configed.");
+    }
+    configs.put(TopicConfig.RETENTION_MS_CONFIG, retention_ms.value());
+    if (!retention_bytes.empty()) {
+      configs.put(TopicConfig.RETENTION_BYTES_CONFIG, retention_bytes.value());
+    }
+
+    ac = AdminClient.create(configuration.getProperties());
+    topic_partitions = partitions.value();
+    partition_replications = replications.value();
+  }
+
+  @Override
+  protected int haveFun() throws Exception {
+
+    List<NewTopic> new_topics = new ArrayList<>(topics.value().length);
+    for (String topic : topics.value()) {
+      new_topics.add(new NewTopic(topic, topic_partitions, partition_replications).configs(configs));
+    }
+    CreateTopicsResult ctr = ac.createTopics(new_topics);
+    ctr.all().get();
+    return RETURN_CODE.SUCCESS.code();
+  }
+
+  @Override
+  protected void destroyToy() throws Exception {
+    ac.close(5, TimeUnit.SECONDS);
+  }
+
+}
---
+++ aries-kafka/src/main/java/org/apache/aries/TopicDeletion.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.aries;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.DeleteTopicsResult;
+import org.apache.kafka.clients.admin.DescribeTopicsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.aries.common.IntParameter;
+import org.apache.aries.common.Parameter;
+import org.apache.aries.common.StringArrayParameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class TopicDeletion extends AbstractKafkaToy {
+
+  private final Parameter<String[]> topics =
+      StringArrayParameter.newBuilder("td.delete_topics")
+                          .setRequired().setDescription("Topics to be deleted.")
+                          .addConstraint(v -> v.length >= 1)
+                          .opt();
+  private final Parameter<Integer> partitions_upper =
+      IntParameter.newBuilder("td.partitions_upper_limit").setRequired()
+                  .setDescription("each deletions will not delete more than this number of partitions")
+                  .opt();
+
+  private AdminClient ac;
+
+  @Override
+  protected void requisite(List<Parameter> requisites) {
+    requisites.add(topics);
+    requisites.add(partitions_upper);
+  }
+
+  @Override
+  protected void buildToy(ToyConfiguration configuration) throws Exception {
+    ac = AdminClient.create(configuration.getProperties());
+  }
+
+  @Override
+  protected int haveFun() throws Exception {
+    List<String> on_list = Arrays.asList(topics.value());
+    List<String> to_be_deleted = new ArrayList<>();
+    int upper_limit = partitions_upper.value();
+
+    DescribeTopicsResult describe_result = ac.describeTopics(on_list);
+    Map<String, KafkaFuture<TopicDescription>> results = describe_result.values();
+    for (String topic : on_list) {
+      TopicDescription topic_description = null;
+      try {
+        topic_description = results.get(topic).get();
+      } catch (Exception utope) {
+        System.out.println(topic + " doesn't exist");
+        continue;
+      }
+      if (topic_description.isInternal()) {
+        System.out.println("Skipping internal topic " + topic);
+        continue;
+      }
+      if (upper_limit > 0) {
+        int num_partitions = topic_description.partitions().size();
+        System.out.println("Topic " + topic + " has " + num_partitions + " partitions");
+        System.out.println("Deleting topic " + topic);
+        to_be_deleted.add(topic);
+        upper_limit -= num_partitions;
+      }
+      if (upper_limit <= 0) break;
+    }
+
+    DeleteTopicsResult delete_result = ac.deleteTopics(to_be_deleted);
+    delete_result.all().get();
+    return RETURN_CODE.SUCCESS.code();
+  }
+
+  @Override
+  protected void destroyToy() throws Exception {
+    ac.close(5, TimeUnit.SECONDS);
+  }
+
+}
