daf605e5ee6ba3d6d9b6b5693f8789eb71a118b8
Reid Chan <reidchan@apache.org>
2021-04-20 18:19:56 +0800

    Add new attribute for CreateTable

--- aries-hbase/src/main/java/org/apache/aries/CreateTable.java
+++ aries-hbase/src/main/java/org/apache/aries/CreateTable.java
@@ -61,6 +61,8 @@ public class CreateTable extends AbstractHBaseToy {
       StringParameter.newBuilder("ct.table_owners").setRequired().setDescription("Whom the table is under in charge by, delimited by ','").opt();
   private final Parameter<Integer> table_split_size =
       IntParameter.newBuilder("ct.table_split_size_in_megabytes").setDescription("The MAX_FILESIZE of a table, larger than this will trigger split.").opt();
+  private final Parameter<Boolean> sensitive_data =
+      BoolParameter.newBuilder("ct.sensitive_data", false).setDescription("Whether the table stores sensitive data").opt();
   // Column family parameters
   // Following are optionals
   private final Parameter<Enum> compression =
@@ -109,6 +111,7 @@ public class CreateTable extends AbstractHBaseToy {
     requisites.add(dec_split_regions);
     requisites.add(table_owners);
     requisites.add(table_split_size);
+    requisites.add(sensitive_data);
     // Family parameters
     requisites.add(compression);
     requisites.add(cache_data_on_write);
@@ -174,6 +177,9 @@ public class CreateTable extends AbstractHBaseToy {
   private HTableDescriptor buildTableDescriptor() {
     HTableDescriptor descriptor = new HTableDescriptor(table);
     descriptor.setValue(Bytes.toBytes("TABLE_OWNERS"), Bytes.toBytes(table_owners.value()));
+    if (sensitive_data.value()) {
+      descriptor.setValue(Bytes.toBytes("SENSITIVE_DATA"), Bytes.toBytes("TRUE"));
+    }
     return descriptor;
   }
 
